81a60c32df2b28501b3c18eaef181d7bc23ac0c6
hintonbradley <hintonbradley@gmail.com>
2020-05-17 11:53:26 -0700

    started oauth

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-})
+// creating an instance of the passport strategy
+passport.use(new GoogleStrategy());
+
+// app.get('/', (req, res) => {
+//     res.send({hi: 'there'});
+// })
 
 let PORT =  process.env.PORT || 5000;
 
