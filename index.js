74be4213757886c12a7d99083ed214fa77f57175
hintonbradley <hintonbradley@gmail.com>
2020-05-17 14:46:17 -0700

    updated server with google auth

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./config/keys')
 
 const app = express();
 
 // creating an instance of the passport strategy
-passport.use(new GoogleStrategy());
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        // Adding route to redirect user after auth has been approved
+        callbackURL: '/auth/google/callback', 
+        // adding final argument to method 
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log('profile is: ', profile);
+    })
+);
 
-// app.get('/', (req, res) => {
-//     res.send({hi: 'there'});
-// })
+app.get(
+    '/auth/google', 
+    passport.authenticate(
+        'google', 
+        {scope: ['profile','email']}
+    )
+);
+
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google')
+)
 
 let PORT =  process.env.PORT || 5000;
 
