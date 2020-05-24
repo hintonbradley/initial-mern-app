8844cb2240cbde3434ec5fe1c356dc4f1767a4b9
hintonbradley <hintonbradley@gmail.com>
2020-05-24 15:36:27 -0700

    added router and material-css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Nav from './Nav';
+import Home from './Home';
+import Dashboard from './Dashboard';
+import SurveyNew from './SurveyNew';
 
 const App = () => {
     return (
-        <div>
-            Hi there!
+        <div className="container">
+            <Nav />
+            <BrowserRouter>
+                <Route path="/" exact component={Home}/>
+                <Route path="/surveys" exact component={Dashboard}/>
+                <Route path="/surveys/new" component={SurveyNew}/>
+            </BrowserRouter>
         </div>
     );
 };
