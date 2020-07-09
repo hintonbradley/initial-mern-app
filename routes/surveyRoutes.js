d2936d8211735dec44b80b09097f1fbf54936600
hintonbradley <hintonbradley@gmail.com>
2020-07-09 16:06:37 -0700

    credit issue resolved

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireCredits');
-const requireCredits = require('../middlewares/requireLogin');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    console.log('')
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    app.post('/api/surveys', 
+    requireLogin, 
+    requireCredits, 
+    (req, res) => {
         const {title, subject, body, recipients } = req.body;
-        console.log('past middleware!')
         const survey = new Survey({
             title,
             subject,
