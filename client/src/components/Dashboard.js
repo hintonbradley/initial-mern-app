35172ae7003b5f47d9571880ef40511b04f1bb6c
hintonbradley <hintonbradley@gmail.com>
2020-07-28 15:18:18 -0700

    mvp finished

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,14 @@
 // import React, {Component} from 'react';
 import React from 'react'
 import {Link} from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
 // class Dashboard extends Component {
 const Dashboard = () => {
     // render() {
         return (
             <div>
-                <h1>Dashboard</h1>
+                < SurveyList />
                 <div className="fixed-action-btn">
                     <Link to="/surveys/new" className="btn-floating btn-large red">
                         <i className="material-icons">add</i>
